76bb2330958fd0a1d510ce19bca292348dba0c69
hellolintao <hellolintao@126.com>
2018-11-22 14:08:05 +0800

    update

--- SUMMARY.md
+++ SUMMARY.md
@@ -1,7 +1,8 @@
 # Summary
 
 * [简介](README.md)
-* [第一章 Vue基础](/Chapter1/section1.md)
-* [第二章 Vue实例与数据绑定1](/Chapter2/section1.md)
+* [第一章 Vue基础](/chapter1/section1.md)
+* [第二章 Vue实例与数据绑定1](/chapter2/section2.md)
 * [第三章 测试章节](README.md)
+
 
---
+++ _book/Chapter7/actual combat/Digital input box/input-number.js
@@ -0,0 +1,20 @@
+Vue.component('input-number',{
+    template:"<div class='input-number'><input/><button>+</button><button>-</button></div>",
+    data:function(){
+        return {
+
+        }
+    },
+    props:{
+
+    },
+    methods:{
+
+    },
+    watch:{
+
+    },
+    mounted:function(){
+
+    }
+});
